4b873c79a7b8e0025d78182c0638a35f38363aab
jnaneshwar das <jnaneshwar.das@gmail.com>
2016-04-05 14:24:43 -0400

    Display name on admin panel

--- webclient/models.py
+++ webclient/models.py
@@ -8,10 +8,16 @@ class CategoryType(models.Model):
     category_name = models.CharField(default='unknown', max_length=100)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
 
+    def __unicode__(self):
+        return 'Category name: ' + self.category_name
+
 
 class ImageSourceType(models.Model):
     description = models.CharField(default='unknown',max_length=200)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __unicode__(self):
+        return 'Description: ' + self.description
 
 
 class Image(models.Model):
@@ -21,12 +27,18 @@ class Image(models.Model):
     source = models.ForeignKey(ImageSourceType, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
 
+    def __unicode__(self):
+        return 'Name: ' + self.name
+
 
 class ImageLabels(models.Model):
     parentImage = models.ForeignKey(Image, on_delete=models.CASCADE)
     categoryType = models.ForeignKey(CategoryType, on_delete=models.CASCADE)
     labelShapes = models.TextField(max_length=10000)
     pub_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __unicode__(self):
+        return 'Category: ' + self.categoryType.category_name
 
 
 
