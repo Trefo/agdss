0fd2e0534cbcf29d83e70391935818ab30c8ef9e
darknight-007 <jnaneshwar.das@gmail.com>
2016-04-09 18:29:51 -0400

    Testing on MacOSX

--- agdss/settings.py
+++ agdss/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/jdas/Dropbox/Research/agriculture/agdss/"
+STATIC_ROOT = "/Users/jdas/Dropbox/Research/agriculture/agdss/"
 
