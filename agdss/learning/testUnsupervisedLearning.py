cdf770dc85b7baa5cffd9f8a41d203033dd7acb3
darknight-007 <jnaneshwar.das@gmail.com>
2016-04-09 18:32:13 -0400

    Testing on MacOSX

--- agdss/learning/testUnsupervisedLearning.py
+++ agdss/learning/testUnsupervisedLearning.py
@@ -6,9 +6,12 @@ image = Image.open('color.png', 'r').getdata()
 imageArray = numpy.fromstring(image.tostring(), dtype='uint8', count=-1, sep='').reshape(image.shape + (len(image.getbands()),))
 
 
-train_set_x =
+train_set_x = ''
+train_set_y = ''
 
 
 f = file('my_data.pkl', 'wb')
 cPickle.dump((train_set_x, train_set_y), f, protocol=cPickle.HIGHEST_PROTOCOL)
 f.close()
+
+# CODE NOT COMPLETE
