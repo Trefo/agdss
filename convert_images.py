5464e3f9cdf5d235ddd3a66b39885d8d41a07815
Sandeep Dcunha <sdcunha@umass.edu>
2016-06-15 16:24:18 -0400

    Added more for converting svg to png

--- convert_images.py
+++ convert_images.py
@@ -1,9 +1,10 @@
-from wand.image import Image
+from wand.image import Image as WandImage
 import StringIO
+from webclient.models import Image, ImageLabel
 
-def convertSVGtoPNG(filepath):
+def convertSVGtoPNG(file):
     #Convert copy of image to new format
-    with Image(file).clone() as img:
+    with WandImage(file).clone() as img:
         img.format = 'png'
         img.save(filename='C:/Users/Sandeep/test.png')
 
@@ -12,8 +13,13 @@ def stringToSVGString(str):
     height = 386
     width = 241
 
+
     return '<?xml version="1.0" encoding="UTF-8" standalone="no"?>' \
        '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">' \
        '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" ' \
        'xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" xml:space="preserve" height=%i ' \
         ' width=%i>%s</svg>' %(height, width, str)
+
+def convertSVGs(LabelList):
+    for label in LabelList:
+        convertSVGtoPNG(stringToSVGString(label.labelShapes))
