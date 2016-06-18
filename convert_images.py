65b669757b8f7fccc8d875b50ee44f2fd748dc2c
Sandeep Dcunha <sdcunha@umass.edu>
2016-06-18 12:23:00 -0400

    Now converts to correct image

--- convert_images.py
+++ convert_images.py
@@ -1,4 +1,5 @@
 from wand.image import Image as WandImage
+from wand.color import Color as WandColor
 import StringIO
 from webclient.models import Image, ImageLabel
 from django.conf import settings
@@ -11,13 +12,20 @@ def convertSVGtoPNG(file, filename):
     if not file:
         #TODO: Some error checking
         return
-    print file.getvalue()
-    print len(file.getvalue())
     try:
         with WandImage(file=file) as img:
+            #img.depth = 8
+            #img.colorspace = 'gray'
+            img.background_color = WandColor('white')
+            img.alpha_channel = 'remove'
+
+            #Convert to black and white
+            img.negate()
+            img.threshold(0)
+            img.negate()
+
             img.format = 'png'
             img.save(filename=(settings.STATIC_ROOT +  'temp/' + filename + '.png'))
-            #img.save(filename=('C:/Users/Sandeep/Dropbox/kumar-prec-ag/test.png'))
             print("converted Image")
     except wand.exceptions.CoderError as e:
         print('Failed to convert: ' + str(e))
@@ -35,13 +43,11 @@ def labelToSVGString(str):
     #Requires encoding as unicode format does not work
     height = result.group('height').encode('utf-8')
     width = result.group('width').encode('utf-8')
-    print height + " " + width
-    print type(height)
-    print type('test')
+
     if height == None or width == None:
         #TODO: Do some error stuff
         return
-    print(str)
+
     SVGStringFile = StringIO.StringIO('<?xml version="1.0" encoding="UTF-8" standalone="no"?>' \
              '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"' \
             ' xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" xml:space="preserve" height="%s"' \
