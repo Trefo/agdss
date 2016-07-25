cd5c86e4e6fa492a7f2b7ba495094f0a59f3a2e3
suryad17 <surya_dhir@asl.org>
2016-07-25 15:29:53 -0400

    Surya Settings File

---
+++ agdss/settings/dev-suryadhir.py
@@ -0,0 +1,7 @@
+from agdss.settings.common import *
+
+
+#Note: Include a '/' at end of folder names
+STATIC_ROOT = "/Users/suryadhir/Desktop/"
+LABEL_FOLDER_NAME='temp/'
+LABEL_AVERAGE_FOLDER_NAME='temp_averages/'
