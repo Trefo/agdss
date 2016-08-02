363a966711b83de1bce54a9ed3a22f1c8fbfef95
suryad17 <surya_dhir@asl.org>
2016-08-02 11:08:22 -0400

    Optimized second stage for iPhone

---
+++ agdss2/settings/prod.py
@@ -0,0 +1,8 @@
+from agdss.settings.common import *
+#DEBUG=False
+
+
+#Note: Include a '/' at end of folder names
+STATIC_ROOT = "/home/sdcunha/Dropbox/kumar-prec-ag/"
+LABEL_FOLDER_NAME='labels/'
+LABEL_AVERAGE_FOLDER_NAME='averages/'
