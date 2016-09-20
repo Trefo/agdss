c21f9e9b2a9909f9942b2c1fc4011f24739e574c
Sandeep Dcunha <sdcunha@umass.edu>
2016-09-20 18:37:02 -0400

    Added first test case

--- webclient/tests.py
+++ webclient/tests.py
@@ -1,2 +1,12 @@
-from django.test import TestCase
+import django.test
+import unittest
+import django.db
 from webclient.models import *
+
+#Models test:
+
+class CategoryTypeModelTestCase(django.test.TestCase):
+
+    def test_category_name_unique(self):
+        CategoryType(category_name="test").save()
+        self.assertRaises(django.db.IntegrityError, lambda: CategoryType(category_name="test").save())
