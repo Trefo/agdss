54ecc1b70014984b52db49e3831e8e044710a11d
jnaneshwar das <jnaneshwar.das@gmail.com>
2017-01-05 15:16:42 -0500

    minor edits

--- agdss/settings/common.py
+++ agdss/settings/common.py
@@ -85,7 +85,7 @@ DATABASES = {
             'NAME': 'agdss',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': 'aguser',
-            'PASSWORD': 'GRASPag2016#',
+            'PASSWORD': 'aguser',
             'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
             'PORT': '',                      # Set to empty string for default.
         }
