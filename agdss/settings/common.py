de01dafb36f4ae0581fd1291e0c107c36c5b7898
Sandeep Dcunha <dcunhas@users.noreply.github.com>
2017-06-27 10:59:11 -0400

    Update database string based on https://stackoverflow.com/questions/2099219/which-postgres-value-should-i-use-in-djangos-database-engine

--- agdss/settings/common.py
+++ agdss/settings/common.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'agdss.wsgi.application'
 
 DATABASES = {
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.postgresql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
             'NAME': 'agdss',                      # Or path to database file if using sqlite3.
             # The following settings are not used with sqlite3:
             'USER': 'aguser',
