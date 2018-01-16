d466c6fe27d0202e231830bc2bc78d7c9e85fcfd
jdas <jnaneshwar.das@gmail.com>
2018-01-15 21:22:17 -0500

    static-root folder edit

--- agdss/settings/common.py
+++ agdss/settings/common.py
@@ -131,7 +131,5 @@ LOGIN_REDIRECT_URL = '/webclient'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = "/code/agdss-1/static-root/"
+STATIC_ROOT = "/app/static-root/"
 CATEGORY_TO_LABEL = 'tomato'
-APPEND_SLASH = False 
-LABEL_FOLDER_NAME = '/labels'
