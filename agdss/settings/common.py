7ecdb52349014b89f7909e2cd155941ad12150c1
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2018-01-16 15:59:28 -0500

    Update common.py

--- agdss/settings/common.py
+++ agdss/settings/common.py
@@ -132,6 +132,6 @@ LOGIN_REDIRECT_URL = '/webclient'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = "/app/static-root/"
-CATEGORY_TO_LABEL = 'ground-gear'
+CATEGORY_TO_LABEL = 'tomato'
 LABEL_FOLDER_NAME = 'labels-'
 
