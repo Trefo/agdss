746fa3438885d8f61ce8e081ff4988d3781259e7
jnaneshwar das <jnaneshwar.das@gmail.com>
2018-06-03 13:34:36 -0400

    reverting default image type to small-tomatoes

--- agdss/settings/common.py
+++ agdss/settings/common.py
@@ -132,5 +132,5 @@ LOGIN_REDIRECT_URL = '/webclient'
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = "/app/static-root/"
-CATEGORY_TO_LABEL = 'almond'
+CATEGORY_TO_LABEL = 'small-tomatoes'
 LABEL_FOLDER_NAME = 'labels/'
